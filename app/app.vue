758c0e89d54163131053d73d7ee53c9e3c4c81c6
Travis Nickels <nickels.travis@gmail.com>
2026-01-14 16:13:53 -0800

    Test connection to the supabase database

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,26 @@
+<script setup>
+import { createClient } from "@supabase/supabase-js";
+
+const config = useRuntimeConfig();
+const supabase = createClient(
+  config.public.supabaseUrl,
+  config.public.supabasePublishableKey
+);
+
+const { data: instruments } = await useAsyncData("instruments", async () => {
+  const { data, error } = await supabase.from("instruments").select();
+  if (error) {
+    console.error("Error fetching instruments:", error);
+    return [];
+  }
+  return data;
+});
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">
+      {{ instrument.name }}
+    </li>
+  </ul>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
-})
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabasePublishableKey: process.env.SUPABASE_PUBLISHABLE_KEY,
+    },
+  },
+
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/image", "@nuxt/ui"],
+});
