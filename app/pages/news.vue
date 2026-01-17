79cb142dc6b70f90a1fb752836b0718b6861e44e
Travis Nickels <nickels.travis@gmail.com>
2026-01-16 23:04:55 -0800

    Add initial pages

---
+++ app/pages/news.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="text-4xl font-semibold tracking-tight">NEWS</h1>
+    <p>Page under construction.</p>
+  </main>
+</template>
---
+++ app/pages/recognitions.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="text-4xl font-semibold tracking-tight">RECOGNITIONS</h1>
+    <p>Page under construction.</p>
+  </main>
+</template>
