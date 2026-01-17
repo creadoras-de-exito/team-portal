a95aed22ae413b47026a4d7f5ce397660e3d72c6
Travis Nickels <nickels.travis@gmail.com>
2026-01-16 23:02:24 -0800

    Add header bar component

---
+++ app/components/header-bar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = ref<NavigationMenuItem[][]>([
+  [
+    { label: 'HOME', to: '/' },
+    { label: 'RECOGNITIONS', to: '/recognitions' },
+    { label: 'NEWS', to: '/news' },
+    { label: 'ABOUT', to: '/about' },
+    { label: 'CONTACT', to: '/contact' }
+  ]
+])
+</script>
+
+<template>
+  <UHeader class="shadow-sm h-24">
+    <template #left>
+      <NuxtLink to="/">
+        <!-- <AppLogo class="w-auto h-6 shrink-0" /> -->
+        <PureBioHealthLogo class="w-auto h-15 pr-20" />
+      </NuxtLink>
+    </template>
+    <UNavigationMenu
+      :items="items"
+      variant="link"
+      class="h hover:text-accent hidden md:flex"
+      :ui="{
+        list: 'gap-8',
+        link: `
+          relative
+          text-lg font-medium
+          after:absolute after:left-0 after:-bottom-1
+          after:h-px after:w-0 after:bg-[color:var(--color-accent)]
+          after:transition-all after:duration-300
+          hover:after:w-full
+        `
+      }"
+      color="info"
+    />
+    <!-- <template #right>
+      <UColorModeButton />
+    </template> -->
+    <template #body>
+      <UNavigationMenu :items="items" variant="link" orientation="vertical" class="space-y-2" color="info" />
+    </template>
+  </UHeader>
+</template>
