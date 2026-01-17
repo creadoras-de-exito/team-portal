70e2218ce1ab0fd8a3563b4bb8a664acea4133e9
Travis Nickels <nickels.travis@gmail.com>
2026-01-16 20:41:36 -0800

    default color mode to light

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/test-utils/module'],
+
   devtools: { enabled: true },
 
   css: ['~/assets/css/main.css'],
+
+  // Remove this if you want to default to system preference
+  colorMode: {
+    preference: 'light',
+    fallback: 'light',
+    classSuffix: '',
+    storageKey: 'color-mode'
+  },
 
   runtimeConfig: {
     public: {
