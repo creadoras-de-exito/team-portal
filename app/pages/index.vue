3d238ce006570d9feeccfad8fd101c81d0ee0bdb
Travis Nickels <nickels.travis@gmail.com>
2026-01-16 20:43:01 -0800

    Update the header on the index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,11 +4,11 @@ const { data: home } = await useAsyncData(() => queryCollection('content').path(
 
 <template>
   <main v-if="home">
-    <h1>{{ home.meta.heroTitle }}</h1>
+    <h1 class="text-4xl font-semibold tracking-tight">{{ home.meta.heroTitle }}</h1>
     <p>{{ home.meta.heroSubtitle }}</p>
 
     <img v-if="home.meta.heroImage" :src="home.meta.heroImage" alt="Hero image" style="max-width: 100%" />
-    <div class="bg-surface p-4">
+    <div class="p-4">
       <UButton color="primary">It works</UButton>
     </div>
   </main>
