2931ef1cb775981d01eafdd0bc0464b4fa3faf61
Travis Nickels <nickels.travis@gmail.com>
2026-01-24 01:08:38 -0800

    Update vitest config to remove deprecated settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,38 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { defineConfig } from 'vitest/config'
+import { fileURLToPath } from 'node:url'
+import { defineVitestProject } from '@nuxt/test-utils/config'
 
-export default defineVitestConfig({
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./app', import.meta.url)),
+      '@': fileURLToPath(new URL('./', import.meta.url))
+    }
+  },
   test: {
     globals: true,
-
-    // default to nuxt environment
-    environment: 'nuxt',
-
-    include: ['tests/unit/**/*.{test,spec}.ts', 'tests/nuxt/**/*.{test,spec}.ts'],
-
-    exclude: ['tests/e2e/**', '**/node_modules/**'],
-
-    // Fall back to node environment for specific tests
-    environmentMatchGlobs: [['tests/unit/**', 'node']],
-
-    setupFiles: ['./tests/setup.ts']
+    setupFiles: ['./tests/setup.ts'],
+    projects: [
+      defineVitestProject({
+        test: {
+          name: 'nuxt',
+          environment: 'nuxt',
+          globals: true,
+          setupFiles: ['./tests/setup.ts'],
+          include: ['tests/nuxt/**/*.{test,spec}.ts'],
+          exclude: ['tests/e2e/**']
+        }
+      }),
+      defineVitestProject({
+        test: {
+          name: 'unit',
+          environment: 'node',
+          globals: true,
+          setupFiles: ['./tests/setup.ts'],
+          include: ['tests/unit/**/*.{test,spec}.ts'],
+          exclude: ['tests/nuxt/**', 'tests/e2e/**']
+        }
+      })
+    ]
   }
 })
