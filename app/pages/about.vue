1105da35d002c5565cbd2135b269ccc1c1dfa0a3
Travis Nickels <nickels.travis@gmail.com>
2026-01-26 00:28:43 -0800

    Add some test to the about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,6 +3,36 @@
 <template>
   <main>
     <h1 class="text-4xl font-semibold tracking-tight">SOBRE</h1>
-    <p>Page under construction.</p>
+    <h2 class="text-3xl font-bold mb-4">Nuestra visión</h2>
+    <p class="mb-4">
+      Nuestra visión es formar una comunidad de líderes conscientes y comprometidos con su crecimiento integral, donde cada persona pueda desarrollar
+      su potencial, fortalecer su identidad y construir una vida abundante con propósito. Visualizamos a Creadoras de Éxito como un espacio de unidad,
+      fe y acción, donde las personas crecen juntas, se apoyan mutuamente y transforman su entorno a través de su desarrollo personal, espiritual y
+      profesional.
+    </p>
+    <h2 class="text-3xl font-bold mb-4">Nuestra misión</h2>
+    <p class="mb-4">
+      Nuestra misión es acompañar y formar a mujeres y caballeros en su crecimiento personal y profesional, brindándoles herramientas, estructura y
+      apoyo para desarrollar su negocio y su liderazgo de manera consciente y alineada a sus valores. Trabajamos desde la fe, la disciplina y la
+      acción constante, creando una comunidad de aprendizaje, acompañamiento y servicio, donde cada persona puede fortalecer su identidad, avanzar con
+      claridad y construir resultados sostenibles sin perder su esencia.
+    </p>
+    <h2 class="text-3xl font-bold mb-4">Nuestros valores</h2>
+    <p class="mb-4">
+      <div class="font-bold">Fe con acción Creemos</div> 
+      <div>en Dios y actuamos con responsabilidad, disciplina y coherencia.</div>
+      <div class="font-bold">Crecimiento consciente</div> 
+      <div>Elegimos crecer internamente para que los resultados externos sean sostenibles.</div>
+      <div class="font-bold">Servicio con intención</div> 
+      <div>Acompañamos desde el respeto, la empatía y el deseo genuino de aportar valor.</div>
+      <div class="font-bold">Liderazgo humano</div> 
+      <div>Guiamos con el ejemplo, formando personas antes que posiciones.</div>
+      <div class="font-bold">Comunidad y unidad</div> 
+      <div>Crecemos juntos, colaboramos y nos apoyamos sin competencia ni comparación.</div>
+      <div class="font-bold">Integridad y coherencia</div> 
+      <div>Alineamos lo que pensamos, sentimos y hacemos.</div>
+      <div class="font-bold">Propósito y abundancia</div> 
+      <div>Construimos resultados con sentido, entendiendo la abundancia como fruto del orden interno y la acción constante.</div>
+    </p>
   </main>
 </template>
